11a28ca5c4bda81195bf6ba19b7a6e5bc69ff53f
Leandro Mateus <mateus6leandro@gmail.com>
2019-09-03 06:19:55 -0300

    Listando horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentsController from './app/controllers/AppointmentsController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -34,4 +35,6 @@ routes.put('/notifications/:id', NotificationController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
 export default routes;
